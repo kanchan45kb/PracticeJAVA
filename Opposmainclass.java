ece2f97d2d66229efc90cb4c913161093b919eb6
kanchan45kb <68049878+kanchan45kb@users.noreply.github.com>
2025-03-24 01:25:04 +0530

    Call class from same package

---
+++ Opposmainclass.java
@@ -0,0 +1,23 @@
+package oopssamepackage;
+import oopsConcept.studentdetail;
+public class Opposmainclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		oopssubclass Studentname = new oopssubclass();
+		Studentname.name ="Mili";
+		Studentname.age=14;
+		Studentname.gender='F';
+		Studentname.div = 'A';
+		Studentname.percentage = 78.23f;
+		Studentname.student();
+		studentdetail stu = new studentdetail();
+		
+		stu.subject();	
+		
+		
+		
+
+	}
+
+}
---
+++ oopssubclass.java
@@ -0,0 +1,22 @@
+package oopssamepackage;
+
+public class oopssubclass {
+	String name;
+	int age;
+	char gender;
+	char div;
+	float percentage;
+	void student()  // Method
+	{ 	
+		
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(gender);
+		System.out.println(div);
+		System.out.println(percentage);
+	}
+	
+		
+	}
+
+
