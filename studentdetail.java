1e1a99310ef56613726199f52ee881f1959fe8ca
kanchan45kb <68049878+kanchan45kb@users.noreply.github.com>
2025-03-24 01:26:09 +0530

    Class  from different package

---
+++ studentdetail.java
@@ -0,0 +1,23 @@
+package oopsConcept;
+
+public class studentdetail {	
+	int subjects = 4;
+	char Grade='A';
+	int mark = 444;
+     
+	public void subject()
+	{
+		
+			
+			System.out.println("How many subjects are ?" +subjects);
+			System.out.println("What is greade " +Grade);
+			System.out.println("Total marks obtain " +mark);
+			
+		
+	
+	}
+	
+
+	}
+
+
