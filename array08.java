d7a8ae05424abdf2290ad77cd132d666a612fa48
kanchan45kb <68049878+kanchan45kb@users.noreply.github.com>
2025-03-29 08:57:58 +0530

    New version: Find second highest number

--- array08.java
+++ array08.java
@@ -10,26 +10,32 @@ public class array08 {
 		Scanner scr = new Scanner(System.in);
 		int l = scr.nextInt();
 		int[] arry= new int[l];
-		System.out.println("Enter array elements");
-		
-		
+		System.out.println("Enter array elements");	
 		for(int i =0;i<l;i++) {
 			arry[i]=scr.nextInt();				
 		}
         
-         System.out.print("|");
+		System.out.print("|");
          for(int m:arry) {
         	 System.out.print(m+" ");
          }
          System.out.println("|");
-        int max = arry[0];
+       int max = arry[0];
         for(int n=0;n<l;n++) {
          if(arry[n]>max) {
         	  max=arry[n];      
         	          }
          }
         System.out.println("Largest number in Array :" +max);
-         
+      
+         int x=-1;
+         for(int k=0;k<l;k++)
+         {
+        	 if(arry[k]>x &&arry[k]!=max) {
+        		 x=arry[k];
+        	 }
+         }
+         System.out.println("Second largest number :" +x);
          
          
 	}
